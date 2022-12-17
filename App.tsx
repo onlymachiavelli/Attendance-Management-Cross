fead5063e0c8c0221fc5199a2e3d8383dd49107e
onlymachiavelli <tmakaveli643@gmail.com>
2022-12-17 17:51:21 +0100

    preparing stacks and pages

--- App.tsx
+++ App.tsx
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View } from "react-native"
+import { NavigationContainer } from "@react-navigation/native"
+import { createStackNavigator } from "@react-navigation/stack"
+import LoginUI from "./src/pages/Login"
+const Stack = createStackNavigator()
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginUI} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/pages/Login.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { View, ScrollView, Text } from "react-native"
+
+const LoginUI = () => {
+  return (
+    <View>
+      <Text>LOGIN UI</Text>
+    </View>
+  )
+}
+
+export default LoginUI
